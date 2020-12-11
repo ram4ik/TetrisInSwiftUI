cc237f95bc49fa7fa8802dad82774ff7acc4adc9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-11 09:59:59 +0200

    Initial Commit

---
+++ TetrisInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TetrisInSwiftUI
+//
+//  Created by ramil on 11.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TetrisInSwiftUI/TetrisInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisInSwiftUIApp.swift
+//  TetrisInSwiftUI
+//
+//  Created by ramil on 11.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TetrisInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
