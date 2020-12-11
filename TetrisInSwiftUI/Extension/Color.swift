ab6e3b38a80692f1f6e4c024e48374e0515c0f53
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-11 10:40:16 +0200

    project init

---
+++ TetrisInSwiftUI/Extension/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  TetrisInSwiftUI
+//
+//  Created by ramil on 11.12.2020.
+//
+
+import SwiftUI
+
+extension Color {
+    public static var tetrisBlack = Color(red: 0/255, green: 0/255, blue: 0/255)
+    public static var tetrisRed = Color(red: 255/255, green: 16/255, blue: 60/255)
+}
