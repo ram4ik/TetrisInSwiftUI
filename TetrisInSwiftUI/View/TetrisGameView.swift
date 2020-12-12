d6268ca310343d832ffb389552059cb1587040b2
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-12-12 11:05:09 +0200

    final view

--- TetrisInSwiftUI/View/TetrisGameView.swift
+++ TetrisInSwiftUI/View/TetrisGameView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct TetrisGameView: View {
-    @ObservedObject var  tetrisGame = TetrisGameViewModel()
+    @ObservedObject var tetrisGame = TetrisGameViewModel()
     
     var body: some View {
         GeometryReader { (geometry: GeometryProxy) in
             self.drawBoard(boundingRect: geometry.size)
         }
+        .gesture(tetrisGame.getMoveGesture())
+        .gesture(tetrisGame.getRotateGesture())
     }
     
     func drawBoard(boundingRect: CGSize) -> some View {
@@ -22,9 +24,10 @@ struct TetrisGameView: View {
         let blocksize = min(boundingRect.width/CGFloat(columns), boundingRect.height/CGFloat(rows))
         let xoffset = (boundingRect.width - blocksize*CGFloat(columns))/2
         let yoffset = (boundingRect.height - blocksize*CGFloat(rows))/2
+        let gameBoard = self.tetrisGame.gameBoard
         
-        return ForEach(0...columns-1, id: \.self) { (column: Int) in
-            ForEach(0...rows-1, id: \.self) { (row: Int) in
+        return ForEach(0...columns-1, id:\.self) { (column:Int) in
+            ForEach(0...rows-1, id:\.self) { (row:Int) in
                 Path { path in
                     let x = xoffset + blocksize * CGFloat(column)
                     let y = boundingRect.height - yoffset - blocksize*CGFloat(row+1)
@@ -32,10 +35,7 @@ struct TetrisGameView: View {
                     let rect = CGRect(x: x, y: y, width: blocksize, height: blocksize)
                     path.addRect(rect)
                 }
-                .fill(self.tetrisGame.gameBoard[column][row].color)
-                .onTapGesture {
-                    tetrisGame.squareClicked(row: row, column: column)
-                }
+                .fill(gameBoard[column][row].color)
             }
         }
     }
--- TetrisInSwiftUI/ViewModel/TetrisGameViewModel.swift
+++ TetrisInSwiftUI/ViewModel/TetrisGameViewModel.swift
@@ -6,27 +6,161 @@
 //
 
 import SwiftUI
+import Combine
 
 class TetrisGameViewModel: ObservableObject {
-    var numRows: Int
-    var numColumns: Int
-    @Published var gameBoard: [[TetrisGameSquare]]
+    @Published var tetrisGameModel = TetrisGameModel()
     
-    init(numRows: Int = 23, numColumns: Int = 10) {
-        self.numRows = numRows
-        self.numColumns = numColumns
+    var numRows: Int { tetrisGameModel.numRows }
+    var numColumns: Int { tetrisGameModel.numColumns }
+    var gameBoard: [[TetrisGameSquare]] {
+        var board = tetrisGameModel.gameBoard.map { $0.map(convertToSquare) }
         
-        gameBoard = Array(repeating: Array(repeating: TetrisGameSquare(color: Color.tetrisBlack), count: numRows), count: numColumns)
+        if let shadow = tetrisGameModel.shadow {
+            for blockLocation in shadow.blocks {
+                board[blockLocation.column + shadow.origin.column][blockLocation.row + shadow.origin.row] = TetrisGameSquare(color: getShadowColor(blockType: shadow.blockType))
+            }
+        }
+        
+        if let tetromino = tetrisGameModel.tetromino {
+            for blockLocation in tetromino.blocks {
+                board[blockLocation.column + tetromino.origin.column][blockLocation.row + tetromino.origin.row] = TetrisGameSquare(color: getColor(blockType: tetromino.blockType))
+            }
+        }
+        
+        return board
     }
     
-    func squareClicked(row: Int, column: Int) {
-        
-        print("Column: \(column), Row: \(row)")
-        if gameBoard[column][row].color == Color.tetrisBlack {
-            gameBoard[column][row].color = Color.tetrisRed
-        } else {
-            gameBoard[column][row].color = Color.tetrisBlack
+    var anyCancellable: AnyCancellable?
+    var lastMoveLocation: CGPoint?
+    var lastRotateAngle: Angle?
+    
+    init() {
+        anyCancellable = tetrisGameModel.objectWillChange.sink {
+            self.objectWillChange.send()
         }
+    }
+    
+    func convertToSquare(block: TetrisGameBlock?) -> TetrisGameSquare {
+        return TetrisGameSquare(color: getColor(blockType: block?.blockType))
+    }
+    
+    func getColor(blockType: BlockType?) -> Color {
+        switch blockType {
+        case .i:
+            return .tetrisLightBlue
+        case .j:
+            return .tetrisDarkBlue
+        case .l:
+            return .tetrisOrange
+        case .o:
+            return .tetrisYellow
+        case .s:
+            return .tetrisGreen
+        case .t:
+            return .tetrisPurple
+        case .z:
+            return .tetrisRed
+        case .none:
+            return .tetrisBlack
+        }
+    }
+    
+    func getShadowColor(blockType: BlockType) -> Color {
+        switch blockType {
+        case .i:
+            return .tetrisLightBlueShadow
+        case .j:
+            return .tetrisDarkBlueShadow
+        case .l:
+            return .tetrisOrangeShadow
+        case .o:
+            return .tetrisYellowShadow
+        case .s:
+            return .tetrisGreenShadow
+        case .t:
+            return .tetrisPurpleShadow
+        case .z:
+            return .tetrisRedShadow
+        }
+    }
+    
+    func getRotateGesture() -> some Gesture {
+        let tap = TapGesture()
+            .onEnded({self.tetrisGameModel.rotateTetromino(clockwise: true)})
+        
+        let rotate = RotationGesture()
+            .onChanged(onRotateChanged(value:))
+            .onEnded(onRotateEnd(value:))
+        
+        return tap.simultaneously(with: rotate)
+    }
+    
+    func onRotateChanged(value: RotationGesture.Value) {
+        guard let start = lastRotateAngle else {
+            lastRotateAngle = value
+            return
+        }
+        
+        let diff = value - start
+        if diff.degrees > 10 {
+            tetrisGameModel.rotateTetromino(clockwise: true)
+            lastRotateAngle = value
+            return
+        } else if diff.degrees < -10 {
+            tetrisGameModel.rotateTetromino(clockwise: false)
+            lastRotateAngle = value
+            return
+        }
+    }
+    
+    func onRotateEnd(value: RotationGesture.Value) {
+        lastRotateAngle = nil
+    }
+    
+    func getMoveGesture() -> some Gesture {
+        return DragGesture()
+        .onChanged(onMoveChanged(value:))
+        .onEnded(onMoveEnded(_:))
+    }
+    
+    func onMoveChanged(value: DragGesture.Value) {
+        guard let start = lastMoveLocation else {
+            lastMoveLocation = value.location
+            return
+        }
+        
+        let xDiff = value.location.x - start.x
+        if xDiff > 10 {
+            print("Moving right")
+            let _ = tetrisGameModel.moveTetrominoRight()
+            lastMoveLocation = value.location
+            return
+        }
+        if xDiff < -10 {
+            print("Moving left")
+            let _ = tetrisGameModel.moveTetrominoLeft()
+            lastMoveLocation = value.location
+            return
+        }
+        
+        let yDiff = value.location.y - start.y
+        if yDiff > 10 {
+            print("Moving Down")
+            let _ = tetrisGameModel.moveTetrominoDown()
+            lastMoveLocation = value.location
+            return
+        }
+        if yDiff < -10 {
+            print("Dropping")
+            tetrisGameModel.dropTetromino()
+            lastMoveLocation = value.location
+            return
+        }
+    }
+    
+    func onMoveEnded(_: DragGesture.Value) {
+        lastMoveLocation = nil
     }
 }
 
